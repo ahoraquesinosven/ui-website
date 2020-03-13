3e95f45eecb8f3ec1ba460ca5f972d1345e7275e
Gisela Morinigo <gisemo@gmail.com>
2020-03-13 19:56:32 -0300

    Move header as a separated component

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,6 +2,21 @@ import Link from "next/link";
 
 import 'bootstrap/dist/css/bootstrap.css';
 
+const Header = () => (
+  <nav className='navbar navbar-expand-lg sticky-top navbar-dark bg-primary'>
+    <div className='container'>
+      <Link href='/'>
+        <a className='navbar-brand'>Observatorio</a>
+      </Link>
+      <a class="nav-item nav-link text-light" href="./report">Informes</a>
+      <a class="nav-item nav-link text-light" href="./campaign">Campañas</a>
+      <a class="nav-item nav-link text-light" href="./mediapresence">En Los Medios</a>
+      <a class="nav-item nav-link text-light" href="./activity">Actividades</a>
+      <a class="nav-item nav-link text-light" href="./about">Quiénes Somos</a>
+   </div>
+  </nav>
+);
+
 const Footer = () => (
   <footer className='container'>
     <strong>TODO</strong>: Implement this footer
@@ -10,18 +25,7 @@ const Footer = () => (
 
 const Application = ({Component, pageProps}) => (
   <>
-    <nav className='navbar navbar-expand-lg sticky-top navbar-dark bg-primary'>
-      <div className='container'>
-        <Link href='/'>
-          <a className='navbar-brand'>Observatorio</a>
-        </Link>
-        <a class="nav-item nav-link text-light" href="./report">Informes</a>
-        <a class="nav-item nav-link text-light" href="./campaign">Campañas</a>
-        <a class="nav-item nav-link text-light" href="./mediapresence">En Los Medios</a>
-        <a class="nav-item nav-link text-light" href="./activity">Actividades</a>
-        <a class="nav-item nav-link text-light" href="./about">Quiénes Somos</a>
-     </div>
-    </nav>
+    <Header />
     <section className='container'>
       <Component {...pageProps} />
     </section>

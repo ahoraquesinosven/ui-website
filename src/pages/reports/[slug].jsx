a183121b6455b5a4be87e2805ce2db7ea49d410c
Gisela Morinigo <gisemo@gmail.com>
2020-04-25 23:11:01 -0300

    Add folder reports, move index inside that folder, added a copy of activity slug and create access to reports data

---
+++ src/pages/reports/[slug].jsx
@@ -0,0 +1,79 @@
+import { faCalendarDay, faMapMarkedAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import moment from 'moment';
+import Link from 'next/link';
+import Breadcrumb from 'react-bootstrap/Breadcrumb';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Row from 'react-bootstrap/Row';
+import Markdown from '../../components/markdown-renderer';
+import OptionalImage from '../../components/optional-image';
+import { fetchActivity } from '../../data/activities';
+
+moment.locale('es');
+
+const ActivityBreadcrumbs = ({ report }) => (
+  <Breadcrumb>
+    <Link href="/" passHref>
+      <Breadcrumb.Item>Inicio</Breadcrumb.Item>
+    </Link>
+    <Link href="/reports" passHref>
+      <Breadcrumb.Item>Informes</Breadcrumb.Item>
+    </Link>
+    <Link href={{ pathname: '/reports', query: { category: report.category.slug } }} passHref>
+      <Breadcrumb.Item>{report.category.title}</Breadcrumb.Item>
+    </Link>
+    <Breadcrumb.Item active>{report.title}</Breadcrumb.Item>
+  </Breadcrumb>
+);
+
+const ActivityDetails = ({ activity }) => {
+  const encodedLocation = encodeURIComponent(activity.location);
+  const mapUrl = `https://www.google.com/maps/search/?api=1&query=${encodedLocation}`;
+  return (
+    <ListGroup variant="flush">
+      <ListGroup.Item>
+        <h5>¿Dónde?</h5>
+        <a rel="noopener noreferrer" target="_blank" href={mapUrl}>
+          <FontAwesomeIcon icon={faMapMarkedAlt} fixedWidth className="mr-2" />
+          {activity.location}
+        </a>
+      </ListGroup.Item>
+      <ListGroup.Item>
+        <h5>¿Cuándo?</h5>
+        <FontAwesomeIcon icon={faCalendarDay} fixedWidth className="mr-2" />
+        {moment(activity.activityDateTime).format('LLL')}
+      </ListGroup.Item>
+    </ListGroup>
+  );
+};
+
+const Activity = ({ activity }) => (
+  <Container className="mt-2">
+    <ActivityBreadcrumbs activity={activity} />
+    <h1>{activity.title}</h1>
+    <p>{activity.summary}</p>
+    <Row>
+      <Col lg={3}>
+        <ActivityDetails activity={activity} />
+      </Col>
+      <Col>
+        <OptionalImage image={activity.mainImage} fluid rounded className="mb-3 content-image" />
+        <Markdown source={activity.content} />
+      </Col>
+    </Row>
+  </Container>
+);
+
+export default Activity;
+
+export async function getServerSideProps({ params }) {
+  const activity = await fetchActivity(params.slug);
+
+  return {
+    props: {
+      activity,
+    },
+  };
+}

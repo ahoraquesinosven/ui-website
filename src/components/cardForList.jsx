f5c09073172285b81ad33e696256c0a6561f6792
Gisela Morinigo <gisemo@gmail.com>
2020-06-28 00:01:36 -0300

    Add CardForList component

---
+++ src/components/cardForList.jsx
@@ -0,0 +1,66 @@
+import Link from 'next/link';
+import Badge from 'react-bootstrap/Badge';
+import Card from 'react-bootstrap/Card';
+import Date from './date';
+
+const contentBaseUrls = {
+  activity: '/activities',
+  media_presence: '/media-presence',
+  report: '/reports',
+  campaign: '/campaigns',
+};
+
+const contentRouting = (content, kind) => {
+  const baseUrl = contentBaseUrls[kind];
+  return {
+    href: `${baseUrl}/[slug]`,
+    as: `${baseUrl}/${content.slug}`,
+  };
+};
+
+const contentKindTranslation = {
+  activity: 'category',
+  media_presence: null,
+  report: 'category',
+  campaign: null,
+};
+
+const CardForList = ({ content, kind, mainDate }) => {
+  let badge = null;
+  if (contentKindTranslation[kind]) {
+    badge = content.category.title;
+  }
+
+  let cardImage = null;
+  if (content.mainImageUrl) {
+    cardImage = <Card.Img variant="top" src={content.mainImageUrl} className="mb-1 fluid rounded" />;
+  } else {
+    cardImage = <Card.Img variant="top" src={content.image.url} className="mb-1 fluid rounded" />;
+  }
+
+  return (
+    <Link {...contentRouting(content, kind)}>
+      <a className="text-primary text-decoration-none">
+        <Card className="shadow p-3 bg-white rounded" style={{ height: '100%' }}>
+          {cardImage}
+          <Card.Body>
+            <Card.Title className="text-uppercase">{content.title}</Card.Title>
+            <p>
+              <Badge variant="primary">{badge}</Badge>
+              &nbsp;|
+              <Badge>
+                <Date className="text-muted" date={mainDate} format={(date) => date.fromNow()} />
+              </Badge>
+            </p>
+            <Card.Text className="text-dark">
+              {content.summary}
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </a>
+    </Link>
+  );
+};
+
+
+export default CardForList;

004affc427553704706d93d816c249f68afc490d
Gisela Morinigo <gisemo@gmail.com>
2020-07-04 00:33:43 -0300

    Add new detail fields to Media presence

--- src/pages/media-presence/[slug].jsx
+++ src/pages/media-presence/[slug].jsx
@@ -1,9 +1,8 @@
-import { faCalendarDay, faNewspaper, faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import moment from 'moment';
 import Link from 'next/link';
 import Container from 'react-bootstrap/Container';
-import ListGroup from 'react-bootstrap/ListGroup';
 import Markdown from '../../components/markdown-renderer';
 import OptionalImage from '../../components/optional-image';
 import { fetchMediaPresence } from '../../data/media-presence';
@@ -11,25 +10,16 @@ import { fetchMediaPresence } from '../../data/media-presence';
 moment.locale('es');
 
 const MediaPresenceDetails = ({ mediaPresence }) => (
-  <ListGroup variant="flush">
-    <ListGroup.Item>
-      <h5>Publicación</h5>
-      <FontAwesomeIcon icon={faCalendarDay} fixedWidth className="mr-2" />
-      {moment(mediaPresence.publicationDate).format('LL')}
-    </ListGroup.Item>
-    <ListGroup.Item>
-      <h5>Medio</h5>
-      <FontAwesomeIcon icon={faNewspaper} fixedWidth className="mr-2" />
-      {mediaPresence.source}
-    </ListGroup.Item>
-    <ListGroup.Item>
-      <h5>Nota Original</h5>
-      <a rel="noopener noreferrer" target="_blank" href={mediaPresence.sourceLink}>
-        <FontAwesomeIcon icon={faExternalLinkAlt} fixedWidth className="mr-2" />
-        Ir a la nota
-      </a>
-    </ListGroup.Item>
-  </ListGroup>
+  <div className="text-center mb-2">
+    <h8 className="pr-1"><b>Publicación:</b></h8>
+    {moment(mediaPresence.publicationDate).format('L')}
+    <h8 className="pr-1 pl-5"><b>Medio:</b></h8>
+    {mediaPresence.source}
+    <a className="pr-1 pl-5" rel="noopener noreferrer" target="_blank" href={mediaPresence.sourceLink}>
+      <FontAwesomeIcon icon={faExternalLinkAlt} fixedWidth className="mr-2" />
+      Ir a la nota
+    </a>
+  </div>
 );
 
 const MediaPresence = ({ mediaPresence }) => (
@@ -40,7 +30,7 @@ const MediaPresence = ({ mediaPresence }) => (
         <a>En los medios</a>
       </Link>
     </div>
-    <OptionalImage image={mediaPresence.mainImage} fluid rounded className="mb-3 content-image" />
+    <OptionalImage image={mediaPresence.mainImage} fluid rounded className="mt-3 mb-3 content-image" />
     <MediaPresenceDetails mediaPresence={mediaPresence} />
     <Markdown source={mediaPresence.content} />
   </Container>

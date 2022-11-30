a965a5bbd8426a32f6f7ddba4d040dfb3904430e
giselamo <gisemo@gmail..com>
2022-11-29 23:22:19 -0300

    Fixes eslint error

--- src/data/api.js
+++ src/data/api.js
@@ -1,12 +1,10 @@
 import getConfig from 'next/config';
 import nodeFetch from 'node-fetch';
 
-const {serverRuntimeConfig} = getConfig();
+const { serverRuntimeConfig } = getConfig();
 
 const apiUrl = (path) => `${serverRuntimeConfig.api.root}/${path}`;
 
-const fetch = (path, options) => {
-  return nodeFetch(apiUrl(path), options);
- };
+const fetch = (path, options) => nodeFetch(apiUrl(path), options);
 
 export default fetch;

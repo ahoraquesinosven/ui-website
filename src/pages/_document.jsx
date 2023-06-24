3f0d07c79965c9323d017e2dcdcf489b9e3c80fb
giselamo <gisemo@gmail..com>
2023-06-24 00:10:10 -0300

    Changes tracking to use GA4

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -26,7 +26,7 @@ class MyDocument extends Document {
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
 
-                    gtag('config', 'UA-171618131-1');
+                    gtag('config', 'G-VLLLC9CQHB');
                 `,
             }}
           />

041642dae7fd0d37676bcd40fb8c83ade3c145d0
Andres Arana <and2arana@gmail.com>
2023-07-25 17:28:15 -0300

    Fixes embed not working on markdown elements
    Due to the way the new react-markdown library was changed on v6, a new
    parsing architecture was implemented which requires a bit of back and
    forth to allow wrapping raw html elements, which we use to add youtube
    video embeds on some content types.

--- src/components/markdown-renderer.jsx
+++ src/components/markdown-renderer.jsx
@@ -1,23 +1,30 @@
 import Markdown from 'react-markdown';
+import rehypeRaw from 'rehype-raw';
+import {toHtml} from 'hast-util-to-html';
 import Image from 'react-bootstrap/Image';
+
+const rehypePlugins = [
+  rehypeRaw,
+];
 
 const components = {
   img: (props) => (
     <Image {...props} fluid rounded className="content-image" />
   ),
-  iframe: ({ value }) => (
-    <div
-      className="embed-responsive embed-responsive-16by9"
-      // eslint-disable-next-line react/no-danger
-      dangerouslySetInnerHTML={{
-        __html: value,
-      }}
-    />
-  ),
+  iframe: (element) => { 
+    return (
+      <div
+        className="embed-responsive embed-responsive-16by9 mb-3"
+        // eslint-disable-next-line react/no-danger
+        dangerouslySetInnerHTML={{
+          __html: toHtml(element.node),
+        }}
+      />
+    ) },
 };
 
 const CustomMarkdownRenderer = ({ children, ...props }) => (
-  <Markdown {...props} components={components}>
+  <Markdown {...props} components={components} rehypePlugins={rehypePlugins}>
     {children}
   </Markdown>
 );
